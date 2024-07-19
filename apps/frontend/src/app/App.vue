591891c580f19d04752504490258d85bc652d67b
djuricmarko <marko.stojanovic1994@gmail.com>
2024-07-19 20:52:51 +0200

    feat: adding favicons and commitlint

--- apps/frontend/src/app/App.vue
+++ apps/frontend/src/app/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts">
-import NxWelcome from './NxWelcome.vue';
-</script>
-
 <template>
-  <NxWelcome title="frontend" />
+  <main class="h-full flex flex-col">
+    <div class="flex justify-between">
+      <h1>CV FORGE</h1>
+      <ul class="flex items-center">
+        <li>
+          <p>Home</p>
+        </li>
+      </ul>
+    </div>
+  </main>
+
+<!--  <NxWelcome title="frontend" />-->
 </template>
