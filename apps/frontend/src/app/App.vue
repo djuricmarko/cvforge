f72afec9c51c0e09061c13c1abf0ecc797d9cb88
djuricmarko <marko.stojanovic1994@gmail.com>
2024-07-23 16:18:33 +0200

    feat: testing

--- apps/frontend/src/app/App.vue
+++ apps/frontend/src/app/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex h-full flex-col">
     <div class="flex justify-between p-5 ">
-      <h1>CV FORGE</h1>
+      <h1>Welcome frontend 👋</h1>
       <ul class="flex items-center">
         <li>
           <p>Home</p>
