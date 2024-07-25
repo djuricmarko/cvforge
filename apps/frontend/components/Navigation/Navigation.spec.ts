8260ec5e95344f5d9d3c3aab6125a049a2f8afee
djuricmarko <marko.stojanovic1994@gmail.com>
2024-07-25 15:52:27 +0200

    refactor: project structure change

--- apps/frontend/components/Navigation/Navigation.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Navigation from './Navigation.vue';
-
-describe('Navigation', () => {
-  it('renders properly', () => {
-    const wrapper = mount(Navigation, {});
-    expect(wrapper.text()).toContain('Welcome to Navigation');
-  });
-});
