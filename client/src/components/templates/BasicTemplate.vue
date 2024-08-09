e4085946be62d60b23b6cd72897353bf29cf1d64
djuricmarko <marko.stojanovic1994@gmail.com>
2024-08-09 10:14:36 +0200

    feat: adding cv templates

---
+++ client/src/components/templates/BasicTemplate.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+const { values } = defineProps<{
+  values?: {
+    name: string,
+    position: string
+  }
+}>();
+</script>
+
+<template>
+  <div class="frame">
+    <div class="header">
+      <h1>{{ values?.name }}</h1>
+      <p>{{ values?.position }}</p>
+    </div>
+    <div class="bar">Bar</div>
+    <div class="main">
+      <div class="sidebar">
+        <div class="sidebar__section">
+          <h2>Contact</h2>
+          <div>
+            <p>Email: </p>
+          </div>
+        </div>
+        <div class="sidebar__section">
+          <h2>Skills</h2>
+          <div>
+            <p>HTML</p>
+          </div>
+        </div>
+        <div class="sidebar__section">
+          <h2>Languages</h2>
+          <div>
+            <p>English (native)</p>
+          </div>
+        </div>
+      </div>
+      <div class="content"/>
+    </div>
+  </div>
+</template>
+
+<style lang="css">
+  .frame {
+    --sidebar-bg-color: #27282D;
+    --bar-bg-color: #E0C692;
+    height: 842px;
+    width: 595px;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header {
+    min-height: 150px;
+    max-height: 150px;
+    flex-grow: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-left: 200px;
+  }
+
+  .header > h1 {
+    font-size: 40px;
+  }
+
+  .header > p {
+    font-size: 16px;
+    color: #616161;
+  }
+
+  .main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+  }
+
+  .bar {
+    width: 100%;
+    background-color: var(--bar-bg-color);;
+    min-height: 65px;
+  }
+
+  .sidebar {
+    height: 100%;
+    width: 200px;
+    background-color: var(--sidebar-bg-color);
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px;
+    color: white;
+  }
+
+  .sidebar__section {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  .sidebar__section h2 {
+    font-size: 12px;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #E0C692;
+  }
+
+  .sidebar__section > div > p {
+    font-size: 11px;
+  }
+</style>
--- client/src/views/Template.vue
+++ client/src/views/Template.vue
@@ -1,3 +1,91 @@
+<script setup lang="ts">
+import { h, Ref, ref } from 'vue';
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { toast } from '@/components/ui/toast';
+import BasicTemplate from '@/components/templates/BasicTemplate.vue';
+
+type FormValues = {
+  name: string,
+  position: string
+}
+
+const formValues: Ref<FormValues> = ref({} as FormValues);
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}));
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = handleSubmit((values) => {
+  toast({
+    title: 'You submitted the following values:',
+    description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
+  });
+});
+
+function handleFormChange(e: Event) {
+  const { value, name } = e?.target as HTMLInputElement;
+  formValues.value = {
+    [name]: value,
+    ...formValues.value,
+  } as FormValues;
+}
+</script>
+
 <template>
-  <h1>Hi</h1>
+  <div class="flex flex-col gap-10 pb-5">
+    <div class="flex h-fit justify-center rounded-xl bg-gray-900 p-5">
+      <BasicTemplate :values="formValues"/>
+    </div>
+    <form class="w-2/3 space-y-6" @submit="onSubmit">
+      <FormField v-slot="{ componentField }" name="name">
+        <FormItem>
+          <FormLabel>Ime i prezime</FormLabel>
+          <FormControl>
+            <Input
+              @keydown="handleFormChange"
+              type="text"
+              placeholder="Vase ime i prezime"
+              v-bind="componentField"
+              v-model="formValues.name"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField }" name="position">
+        <FormItem>
+          <FormLabel>Zanimanje</FormLabel>
+          <FormControl>
+            <Input
+              @keydown="handleFormChange"
+              type="text"
+              placeholder="Zanimanje"
+              v-bind="componentField"
+              v-model="formValues.position"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <Button type="submit">
+        Create your CV
+      </Button>
+    </form>
+  </div>
 </template>
