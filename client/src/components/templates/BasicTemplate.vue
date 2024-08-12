685036893648c22c7b6c774943c6f7f7b1c22cc4
djuricmarko <markodjuric94@proton.me>
2024-08-13 00:23:55 +0200

    feat: basic template

--- client/src/components/templates/BasicTemplate.vue
+++ client/src/components/templates/BasicTemplate.vue
@@ -10,26 +10,51 @@ const { values } = defineProps<{
 <template>
   <div class="frame">
     <div class="header">
-      <h1>{{ values?.name }}</h1>
-      <p>{{ values?.position }}</p>
+      <div class="header__image">Image</div>
+      <div class="header__info">
+        <h1>{{ values?.name }}</h1>
+        <p>{{ values?.position }}</p>
+      </div>
     </div>
-    <div class="bar">Bar</div>
+    <div class="links">
+      <div>
+        <img src="/icons/github.svg" alt="icon">
+        <p>github.com/djuricmarko</p>
+      </div>
+      <div>
+        <img src="/icons/linkedin.svg" alt="icon">
+        <p>linkedin.com/djuricmarko</p>
+      </div>
+      <div>
+        <img src="/icons/mail.svg" alt="icon">
+        <p>marko.stojanovic1994@gmail.com</p>
+      </div>
+    </div>
     <div class="main">
       <div class="sidebar">
         <div class="sidebar__section">
-          <h2>Contact</h2>
+          <div class="sidebar__section__heading">
+            <h2>Personal information</h2>
+            <hr>
+          </div>
           <div>
             <p>Email: </p>
           </div>
         </div>
         <div class="sidebar__section">
-          <h2>Skills</h2>
+          <div class="sidebar__section__heading">
+            <h2>Contact</h2>
+            <hr>
+          </div>
           <div>
             <p>HTML</p>
           </div>
         </div>
         <div class="sidebar__section">
-          <h2>Languages</h2>
+          <div class="sidebar__section__heading">
+            <h2>Contact</h2>
+            <hr>
+          </div>
           <div>
             <p>English (native)</p>
           </div>
@@ -56,17 +81,31 @@ const { values } = defineProps<{
     max-height: 150px;
     flex-grow: 0;
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding-left: 200px;
   }
 
-  .header > h1 {
+  .header__info {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: center;
+    justify-content: center;
+    padding: 0 45px;
+  }
+
+  .header__image {
+    min-width: 200px;
+    height: 100%;
+    background-color: var(--sidebar-bg-color);
+  }
+
+  .header__info > h1 {
     font-size: 40px;
   }
 
-  .header > p {
+  .header__info > p {
     font-size: 16px;
     color: #616161;
   }
@@ -77,10 +116,30 @@ const { values } = defineProps<{
     display: flex;
   }
 
-  .bar {
+  .links {
     width: 100%;
-    background-color: var(--bar-bg-color);;
     min-height: 65px;
+    max-height: 65px;
+    background-color: var(--bar-bg-color);
+    display: flex;
+    gap: 10px;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 0 20px;
+  }
+
+  .links > div {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+
+  .links > div > p {
+    font-size: 11px;
+  }
+
+  .links > div > img {
+    height: 15px;
   }
 
   .sidebar {
@@ -97,7 +156,7 @@ const { values } = defineProps<{
   .sidebar__section {
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    gap: 15px;
   }
 
   .sidebar__section h2 {
@@ -109,5 +168,16 @@ const { values } = defineProps<{
 
   .sidebar__section > div > p {
     font-size: 11px;
+  }
+
+  .sidebar__section__heading {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  .sidebar__section__heading > hr {
+    width: 80px;
+    border-width: 2px;
   }
 </style>
